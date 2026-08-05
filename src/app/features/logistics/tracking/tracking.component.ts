89d5aa330d75bcb7b5abe91be2614d9d441d6060
12130801 <12130801@me.mcu.edu.tw>
2026-08-05 14:04:49 +0800

    docs(logistic-tracking):修改配送查詢

--- src/app/features/logistics/tracking/tracking.component.ts
+++ src/app/features/logistics/tracking/tracking.component.ts
@@ -1,31 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tracking',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tracking.component.html',
   styleUrl: './tracking.component.scss'
 })
 export class TrackingComponent {
+  showResult = false;
 
   keyword = '';
 
   trackingList = [
     {
       id: 'A001',
-      name: '花蓮救災行動',
       status: '配送中'
     },
+    {
+      id: 'A002',
+      status: '已送達'
+    }
   ];
 
   resultList = this.trackingList;
 
   search() {
+
+  // 去除前後空白
+  const keyword = this.keyword.trim();
+
+  // 沒有輸入任何文字
+  if (!keyword) {
+    alert('請先輸入物流單號！');
+    this.showResult = false;
+    return;
+  }
     this.resultList = this.trackingList.filter(item =>
-      item.name.includes(this.keyword) ||
       item.id.includes(this.keyword)
     );
+    this.showResult = true;
   }
 }
