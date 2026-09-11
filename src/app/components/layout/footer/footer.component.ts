f84f8e0b4c3398dad474063fe4249282ae7d32e7
lin0835 <happy.20041221@gmail.com>
2026-09-11 19:07:02 +0800

    feat(layout):調整跑馬燈

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,4 +8,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  isHeaderHidden = false;
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    this.isHeaderHidden = window.scrollY > 70;
+  }
+}
