4340272145b2847d2d043a6584858ef7465c1683
lin0835 <happy.20041221@gmail.com>
2026-09-24 03:23:22 +0800

    feat(about-us):新增聯絡我們頁面

--- src/app/features/about-us/contact-us/contact-us.component.ts
+++ src/app/features/about-us/contact-us/contact-us.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
 })
